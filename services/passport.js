629fb2284faf0bb362415adaf63cd1a674e58d50
Edison Yang <edison.cy.yang@gmail.com>
2018-07-29 01:48:33 -0700

    completed all google OAuth flows on the server side.
    Able to login, logout, and show current user. Utilize passport.js and
    cookieSession to authenticate and identify users.

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -8,8 +21,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token: ', accessToken);
-        console.log('refresh token: ', refreshToken);
-        console.log('profile: ', profile);
+        User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+            if(existingUser) {
+                done(null, existingUser);
+            }
+            else {
+                new User({googleId: profile.id}).save()
+                .then(user => {
+                    done(null, user);
+                })
+            }
+        });
     })
 );
