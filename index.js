cf227d02ce6b71bf70460e4b504cf0306c9ec3f1
Edison Yang <edison.cy.yang@gmail.com>
2018-07-28 21:17:07 -0700

    Passport.js, google strategy and Google keys initial setup

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
+
+//clientID 711594909932-qm2o600h2l5tc0vm0ivj0k2taemqq8ur.apps.googleusercontent.com
+//clientSecret xz7BzKAZJ5ReOJJYmFPgTuNT
 
 app.get('/', (req, res) => {
     res.send({howsLife: 'yez'});
 })
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
