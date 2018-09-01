2f93f326182486807c77fba6728ed48421aa8150
Edison Yang <edison.cy.yang@gmail.com>
2018-09-01 15:30:05 -0700

    Revert "Merge branch 'Express'"
    This reverts commit fcf093f253ae49306a3f929cf57e6e065cfe3a77, reversing
    changes made to 04d0b3571e5b4d3e8aafd5460b7d2ee4b27b78da.

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '711594909932-qm2o600h2l5tc0vm0ivj0k2taemqq8ur.apps.googleusercontent.com',
-    googleClientSecret: 'xz7BzKAZJ5ReOJJYmFPgTuNT',
-    mongoURI: 'mongodb://lebronjames:password123@ds241019.mlab.com:41019/drone-web-dev',
-    cookieKey: 'sergsthdrthdrthrserhenyt'
-};
